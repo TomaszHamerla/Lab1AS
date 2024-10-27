681ed8a29871d5203883ea25935c04cd50cdf6d0
unknown <tomaszhamerla@interia.pl>
2024-10-27 14:17:02 +0100

    added new activity, switch btn

--- app/src/main/java/com/example/lab1as/MainActivity.java
+++ app/src/main/java/com/example/lab1as/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.lab1as;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    private Switch upperCase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        upperCase = findViewById(R.id.switch1);
+    }
+    public void wyslijWiadomosc(View v) {
+        EditText editText = findViewById(R.id.editText1);
+        String message = editText.getText().toString();
+        Intent intent = new Intent(this, NowaAktywnosc.class);
+        if (upperCase.isChecked()) {
+            message = message.toUpperCase();
+        }
+        intent.putExtra("Extra-msg", message);
+        startActivity(intent);
     }
 }
